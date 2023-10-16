b52a444c176f96531df32a2f3ef7913ad193e0c9
ragav5042 <vimala5042@gmail.com>
2023-10-16 19:19:15 +0530

    video streaming added

--- public/script.js
+++ public/script.js
@@ -7,7 +7,24 @@ var peer = new Peer(undefined, {
 });
 
 const user = prompt("Enter your name");
-
+const myvideo= document.createElement("video")
+myvideo.muted=true
+let mystream;
+navigator.mediaDevices.getUserMedia({
+    audio:true,
+    video:true
+})
+.then((stream)=>{
+    mystream=stream
+    addVideoStream(myvideo,stream)
+})
+function addVideoStream  (video,stream){
+    video.srcObject=stream
+    video.addEventListener("loadedmetadata",()=>{
+        video.play()
+        $("video_grid").append(video)
+    })
+}
 $(function () {
     $("#show_chat").click(function () {
         $(".left-window").css("display", "none")
